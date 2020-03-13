d7da13bffebd617e41f13e4df70c12b92ea08d67
mohamed-attia <m.attia@outlook.sa>
2020-03-13 20:34:55 +0300

    Updates

---
+++ precache-manifest.c029368cb84ce422651196adb527b9cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3681420a6ac68afc06faa8cc3c63c57c",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "64a41e6012c78bfa9549",
+    "url": "/react-weather-app/static/css/main.cbdfd04b.chunk.css"
+  },
+  {
+    "revision": "a26ec865b5affa40326e",
+    "url": "/react-weather-app/static/js/2.11eeb9a1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-weather-app/static/js/2.11eeb9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a41e6012c78bfa9549",
+    "url": "/react-weather-app/static/js/main.d36de1a8.chunk.js"
+  },
+  {
+    "revision": "8af5c2074cf92f8d052c",
+    "url": "/react-weather-app/static/js/runtime-main.83989fd5.js"
+  }
+]);
